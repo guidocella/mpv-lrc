af86b8008070afa04960f97e35a2a361a831c92c
Guido Cella <guido@guidocella.xyz>
2021-07-17 19:39:33 +0200

    inline variable

--- lrc-shutdown.lua
+++ lrc-shutdown.lua
@@ -1,7 +1,5 @@
-local utils = require 'mp.utils'
-
 mp.register_event('shutdown', function ()
-    if utils.shared_script_property_get('showed-lyrics') then
+    if require 'mp.utils'.shared_script_property_get('showed-lyrics') then
         os.exit()
     end
 end)
