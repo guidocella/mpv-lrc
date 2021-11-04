eb6cfe3cbe665c4dbecc0a3addeb7141d65209cb
Guido Cella <guido@guidocella.xyz>
2021-11-04 20:41:03 +0100

    use printf

--- lrc.sh
+++ lrc.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-lrc_path=$(echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpv-socket | jq -r .data)
+lrc_path=$(printf %s\\n '{ "command": ["get_property", "path"] }' | socat - /tmp/mpv-socket | jq -r .data)
 [ "$lrc_path" ] || exit 1
 lrc_path=${lrc_path%.*}.lrc
 case $lrc_path in
     /*) ;;
-    *) lrc_path=$(echo '{ "command": ["get_property", "working-directory"] }' | socat - /tmp/mpv-socket | jq -r .data)/$lrc_path
+    *) lrc_path=$(printf %s\\n '{ "command": ["get_property", "working-directory"] }' | socat - /tmp/mpv-socket | jq -r .data)/$lrc_path
 esac
 
 [ -e "$lrc_path" ] && exec $EDITOR "$lrc_path"
 
-metadata=$(echo '{ "command": ["get_property", "metadata"] }' \
+metadata=$(printf %s\\n '{ "command": ["get_property", "metadata"] }' \
     | socat - /tmp/mpv-socket | jq .data)
 # The keys are lower case in ID3 tags and upper case in Vorbis comments.
-artist=$(echo $metadata | jq -r 'if has("artist") then .artist else .ARTIST end')
-title=$(echo $metadata | jq -r 'if has("title") then .title else .TITLE end')
-album=$(echo $metadata | jq -r 'if has("album") then .album else .ALBUM end')
+artist=$(printf %s "$metadata" | jq -r 'if has("artist") then .artist else .ARTIST end')
+title=$(printf %s "$metadata" | jq -r 'if has("title") then .title else .TITLE end')
+album=$(printf %s "$metadata" | jq -r 'if has("album") then .album else .ALBUM end')
 [ "$album" = null ] && album= || album="[album:$album]
 "
-duration=$(echo '{ "command": ["get_property", "duration"] }' | socat - /tmp/mpv-socket | jq .data)
+duration=$(printf %s\\n '{ "command": ["get_property", "duration"] }' | socat - /tmp/mpv-socket | jq .data)
 seconds=${duration%.*}
 length=$(printf %02d $(( seconds / 60 ))):$(printf %02d $(( seconds % 60 )))
 
-echo "[ar:$artist]
+printf %s "[ar:$artist]
 [ti:$title]
 $album[length:$length]
 [by:$1]
@@ -31,7 +31,7 @@ $album[length:$length]
 
 query="$artist $title"
 
-if echo $query | grep -Eiq '([ぁ-ヺ一-龢]|KOTOKO |Ceui )'; then
+if printf %s "$query" | grep -Eiq '([ぁ-ヺ一-龢]|KOTOKO |Ceui )'; then
     # Exclude sites that serve lyrics as images and some non-lyrics sites
     # that Duckduckgo occasionally returns as the first result.
     # When sites try to block copying text, you can inspect the HTML element
@@ -48,7 +48,7 @@ ${BROWSER:-chromium} "https://duckduckgo.com/?q=\\$query" 2>/dev/null &
 if [ "$WAYLAND_DISPLAY" ] && command -v wl-copy >/dev/null; then
     wl-copy -- "$query"
 elif command -v xclip >/dev/null; then
-    echo -n $query | xclip -selection clipboard
+    printf %s "$query" | xclip -selection clipboard
 fi
 
 exec $EDITOR + "$lrc_path"
