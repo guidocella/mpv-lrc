13e133db7ed692bdaea0921e5a7598b6077b5117
Guido Cella <guido@guidocella.xyz>
2025-01-11 11:05:02 +0100

    use normalize-path

--- lrc.sh
+++ lrc.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-lrc_path=$(printf %s\\n '{ "command": ["get_property", "path"] }' | socat - UNIX-CONNECT:/tmp/mpv-socket) || exit 1
+lrc_path=$(printf %s\\n '{ "command": ["expand-properties", "normalize-path", "${path}"] }' | socat - UNIX-CONNECT:/tmp/mpv-socket) || exit 1
 
 lrc_path=$(printf %s "$lrc_path" | jq -r .data)
-[ "$lrc_path" = null ] && exit 1
+case $lrc_path in
+    */'(unavailable)') exit 1 ;;
+esac
 
 lrc_path=${lrc_path%.*}.lrc
-case $lrc_path in
-    /*) ;;
-    *) lrc_path=$(printf %s\\n '{ "command": ["get_property", "working-directory"] }' | socat - /tmp/mpv-socket | jq -r .data)/$lrc_path
-esac
 
 [ -e "$lrc_path" ] && exec $EDITOR "$lrc_path"
 [ -e "${lrc_path%.*}.ja.lrc" ] && exec $EDITOR "${lrc_path%.*}.ja.lrc"
