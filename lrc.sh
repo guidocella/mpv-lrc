94a56a53fb1d52da59dd84bbaf3eeeffbb3c54ea
Guido Cella <guido@guidocella.xyz>
2025-02-14 12:48:59 +0100

    stop altering path now that it's always normalized
    See https://github.com/mpv-player/mpv/commit/38ad1ed03b

--- lrc.sh
+++ lrc.sh
@@ -6,10 +6,6 @@ lrc_path=$(printf %s "$lrc_path" | jq -r .data)
 [ "$lrc_path" = null ] && exit 1
 
 lrc_path=${lrc_path%.*}.lrc
-case $lrc_path in
-    /*) ;;
-    *) lrc_path=$(printf %s\\n '{ "command": ["get_property", "working-directory"] }' | socat - /tmp/mpv-socket | jq -r .data)/$lrc_path
-esac
 
 [ -e "$lrc_path" ] && exec $EDITOR "$lrc_path"
 [ -e "${lrc_path%.*}.ja.lrc" ] && exec $EDITOR "${lrc_path%.*}.ja.lrc"
